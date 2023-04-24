40ea2638a05a1863491466794c718d55f05b4d2d
AshKid <alejandro.sanchezh@campusucc.edu.co>
2023-04-24 18:50:30 -0500

    modificacion scripts

---
+++ BD_TICKET/Scripts/PG_scripts_impuestos.sql
@@ -0,0 +1,10 @@
+/*==============================================================*/
+/* Table: IMPUESTOS                                             */
+/*==============================================================*/
+/*CREATE TABLE IMPUESTOS (
+   ID_IMPUESTO          VARCHAR(5)           NOT NULL,
+   DESCRIPCION          VARCHAR(20)          NULL,
+   PORCENTAJE           DECIMAL              NULL,
+   CONSTRAINT PK_IMPUESTOS PRIMARY KEY (ID_IMPUESTO)
+);*/
+
